7a2f23cbb4e11a92e5d0f4b4378686cbcbe87abe
Łukasz Włódarczyk <lukasz.wlodarczyk@onegini.com>
2018-03-18 19:04:14 +0100

    updated tests

--- src/test/kotlin/com/createam/heroku/thymeleaf/HttpsEnforcerTest.kt
+++ src/test/kotlin/com/createam/heroku/thymeleaf/HttpsEnforcerTest.kt
@@ -3,7 +3,7 @@ package com.createam.heroku.thymeleaf
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Matchers
-import org.mockito.Mockito
+import org.mockito.Mockito.*
 import org.mockito.runners.MockitoJUnitRunner
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
@@ -13,32 +13,33 @@ import javax.servlet.http.HttpServletResponse
 @RunWith(MockitoJUnitRunner::class)
 class HttpsEnforcerTest {
 
-    private val request: HttpServletRequest = Mockito.mock(HttpServletRequest::class.java)
-    private val response: HttpServletResponse = Mockito.mock(HttpServletResponse::class.java)
-    private val chain: FilterChain = Mockito.mock(FilterChain::class.java)
+    private val request: HttpServletRequest = mock(HttpServletRequest::class.java)
+    private val response: HttpServletResponse = mock(HttpServletResponse::class.java)
+    private val chain: FilterChain = mock(FilterChain::class.java)
 
-    private var TEST_HEROKU_URL: String = "for-sure-no-existing-addressaddress.xyz"
-    private var EXPECTED_HEROKU_URL: String = "https://for-sure-no-existing-addressaddress.xyz"
+    private var TEST_HEROKU_URL: String = "for-sure-no-existing-address.xyz"
+    private var EXPECTED_HEROKU_URL: String = "https://for-sure-no-existing-address.xyz"
 
     @Test
     fun shouldEnforceHttpsOnHeroku() {
         val httpsEnforcer = HttpsEnforcer()
-        Mockito.`when`(request.getHeader(Matchers.eq("x-forwarded-proto"))).thenReturn(TEST_HEROKU_URL)
+        `when`(request.getHeader(Matchers.eq("x-forwarded-proto"))).thenReturn(TEST_HEROKU_URL)
+
         httpsEnforcer.doFilter(request, response, chain)
 
-        Mockito.verify(chain, Mockito.times(1)).doFilter(request, response)
+        verify(chain, times(1)).doFilter(request, response)
     }
 
     @Test
     fun shouldRedirectToSameUrlOnHeroku() {
         val httpsEnforcer = HttpsEnforcer()
-        Mockito.`when`(request.getHeader(Matchers.eq("x-forwarded-proto"))).thenReturn(TEST_HEROKU_URL)
-        Mockito.`when`(request.serverName).thenReturn(TEST_HEROKU_URL)
-        Mockito.`when`(request.requestURI).thenReturn("/api")
+        `when`(request.getHeader(Matchers.eq("x-forwarded-proto"))).thenReturn(TEST_HEROKU_URL)
+        `when`(request.serverName).thenReturn(TEST_HEROKU_URL)
+        `when`(request.requestURI).thenReturn("/api")
 
         httpsEnforcer.doFilter(request, response, chain)
 
         val expectedUrl: String = EXPECTED_HEROKU_URL + "/api"
-        Mockito.verify(response, Mockito.times(1)).sendRedirect(expectedUrl)
+        verify(response, times(1)).sendRedirect(expectedUrl)
     }
 }
